d3ff076bf9cc94945eda567a97811a8b0c2012dd
maarten <hoevenvd@gmail.com>
2012-12-15 21:34:09 +0100

    Ooops, pushed an empty db-migrate script to github. Now correct: it changes the field et from integer to float in archive-records.

--- rails/wx-services/db/migrate/20121213210443_change_field_et_archive_records.rb
+++ rails/wx-services/db/migrate/20121213210443_change_field_et_archive_records.rb
@@ -1,7 +1,9 @@
 class ChangeFieldEtArchiveRecords < ActiveRecord::Migration
   def self.up
+    change_column :archive_records, :et, :float,
   end
 
   def self.down
+    change_column :archive_records, :et, :integer,
   end
 end
