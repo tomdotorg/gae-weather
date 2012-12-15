3e27190c8d66859e15c535f21d39fef6c2a45f3e
maarten <hoevenvd@gmail.com>
2012-12-15 21:45:47 +0100

    Pfff, syntax-error in previous version. No good to rush, take your time, relax, verify, verify and verify :). Okay, fixed now. No comma behind float and integer. Duh

--- rails/wx-services/db/migrate/20121213210443_change_field_et_archive_records.rb
+++ rails/wx-services/db/migrate/20121213210443_change_field_et_archive_records.rb
@@ -1,9 +1,9 @@
 class ChangeFieldEtArchiveRecords < ActiveRecord::Migration
   def self.up
-    change_column :archive_records, :et, :float,
+    change_column :archive_records, :et, :float
   end
 
   def self.down
-    change_column :archive_records, :et, :integer,
+    change_column :archive_records, :et, :integer
   end
 end
