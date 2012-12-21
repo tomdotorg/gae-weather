0c96b061769af32360d44403649d2f0c5410f199
Tom Mitchell <tom@tom.org>
2012-12-20 19:54:19 -0500

    added daylight to view

--- rails/wx-webapp/app/models/riseset.rb
+++ rails/wx-webapp/app/models/riseset.rb
@@ -2,9 +2,9 @@ class Riseset < ActiveRecord::Base
   def self.riseset(location, date)
     r = Riseset.find_by_location_and_month_and_day(location,
             date.localtime.month, date.localtime.day)
-    t = { :rise => Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min),
-          :set => Time.gm(date.year, date.month, date.day, r.set.hour, r.set.min) }
-    t[:set] = t[:set] + 1.day if t[:set].hour < 12
+    rise = Time.gm(date.year, date.month, date.day, r.rise.hour, r.rise.min)
+    set = Time.gm(date.year, date.month, (r.set.hour < r.rise.hour) ? date.day + 1 : date.day, r.set.hour, r.set.min)
+    t = { :rise => rise, :set => set, :daylight => daylight(rise, set) }
     return t
   end
 
@@ -14,6 +14,24 @@ class Riseset < ActiveRecord::Base
     r = Riseset.riseset(location, date)
     #find_by_location_and_month_and_day(AppConfig.climate_location, date.month, date.day)
     return ((date < r[:rise] - 1.hour) or (date >= r[:set] + 1.hour))
+  end
+
+  SECONDS_IN_AN_HOUR = 60 * 60
+
+  # send it in in localtime to avoid logic around UTC
+  # TODO - have this handle end times that are the next day
+  # (only a problem if time is a time only and not a date/time type)
+
+  def self.daylight(start_time, end_time)
+    if end_time < start_time
+      raise ArgumentError, 'end is earlier than start'
+    end
+
+    interval = end_time - start_time
+    hours = (interval / SECONDS_IN_AN_HOUR).to_i
+    mins = ((interval % SECONDS_IN_AN_HOUR) / 60).to_i
+    tmp = Time.now
+    return Time.gm(tmp.year, tmp.month, tmp.day, hours, mins)
   end
 end
 #t = Time.gm(Time.now.year, Time.now.month, Time.now.day, r[:set].hour, r[:set].min).localtime
