d9aca91f0025ce483176a2e760ecd6a75ca41712
Tom Mitchell <tom@tom.org>
2012-12-24 08:24:20 -0500

    sort forecast periods on pdnum instead of relying on the luck of generated id

--- rails/wx-services/app/models/wunder_forecast.rb
+++ rails/wx-services/app/models/wunder_forecast.rb
@@ -1,6 +1,6 @@
 class WunderForecast < ActiveRecord::Base
   unloadable # hack to prevent errors about can't dup Nil.class (http://strd6.com/?p=250)
-  has_many  :wunder_forecast_periods, { :dependent => :delete_all, :order => :id }
+  has_many  :wunder_forecast_periods, { :dependent => :delete_all, :order => :pdnum }
   has_many  :wunder_forecast_period_longs, { :dependent => :delete_all, :order => :id }
 
   named_scope :latest_priv, lambda { |location| {:conditions => ["location = ?", location]} }
