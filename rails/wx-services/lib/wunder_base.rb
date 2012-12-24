c683b15f76ebc657a903fbe6c340c247de274aa6
Tom Mitchell <tom@tom.org>
2012-12-24 06:38:16 -0500

    change noaa_locatio to wunderground_location

--- rails/wx-services/lib/wunder_base.rb
+++ rails/wx-services/lib/wunder_base.rb
@@ -3,7 +3,7 @@ require 'wunderground'
 
 class WunderBase
   def initialize
-    @location = ARGV[0] ||= AppConfig.noaa_location
+    @location = ARGV[0] ||= AppConfig.wunderground_location
     @key = AppConfig.wunderground_key
     @api = Wunderground.new(@key)
     @api.language = AppConfig.wunderground_lang ||= "EN"
