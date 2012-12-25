f379ae153ed17f0a9a1b8748f804ca469ef6c641
Tom Mitchell <tom@tom.org>
2012-12-25 10:56:51 -0500

    minor updates - trivial

--- rails/wx-webapp/test/functional/wx_controller_test.rb
+++ rails/wx-webapp/test/functional/wx_controller_test.rb
@@ -3,11 +3,8 @@ require 'test_helper'
 class WxControllerTest < ActionController::TestCase
 #  fixtures :current_conditions
 
-#  def test_index
-#    a = create_a_good_struct
-#    invoke :put_current_conditions, AppConfig.service_password, AppConfig.location, a
-
-#    get :index
-#    assert_response :success
-#  end
+  def test_index
+    get :index
+    assert_response :success
+  end
 end
