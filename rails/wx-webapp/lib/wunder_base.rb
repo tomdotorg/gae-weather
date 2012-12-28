32f05b93192693c90566585dd2a09f329c06e1ad
Tom Mitchell <tom@tom.org>
2012-12-28 06:55:06 -0500

    no command line option in webapp case

--- rails/wx-webapp/lib/wunder_base.rb
+++ rails/wx-webapp/lib/wunder_base.rb
@@ -3,7 +3,7 @@ require 'wunderground'
 
 class WunderBase
   def initialize
-    @location = ARGV[0] ||= AppConfig.wunderground_location
+    @location = AppConfig.wunderground_location
     @key = AppConfig.wunderground_key
     @api = Wunderground.new(@key)
     @api.language = AppConfig.wunderground_lang ||= "EN"
